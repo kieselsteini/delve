4699a96a26725cb60d86318bc37041bfca8b220a
Sebastian Steinhauer <s.steinhauer@yahoo.de>
2019-06-15 14:32:21 +0200

    Removed doubled errno.h and bumped version

--- delve.c
+++ delve.c
@@ -20,7 +20,6 @@
 ================================================================================
 */
 /*============================================================================*/
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -947,7 +946,7 @@ int main(int argc, char **argv) {
 	(void)argc; (void)argv;
 
 	puts(
-		"delve - 0.7.6  Copyright (C) 2019  Sebastian Steinhauer\n" \
+		"delve - 0.7.7  Copyright (C) 2019  Sebastian Steinhauer\n" \
 		"This program comes with ABSOLUTELY NO WARRANTY; for details type `help license'.\n" \
 		"This is free software, and you are welcome to redistribute it\n" \
 		"under certain conditions; type `help license' for details.\n" \
